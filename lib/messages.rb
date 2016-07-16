6f22c6ab49216f0ed939780f9598c431fcda8749
Ben Pepper <bfpepper87@gmail.com>
2016-07-15 18:00:02 -0600

    added first tests and classes

---
+++ lib/messages.rb
@@ -0,0 +1,25 @@
+class Message
+
+  def welcome
+    "Welcome to BATTLESHIP\n\n
+    Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
+  end
+
+  def instructions
+    "You are playing against a very simple computer.
+
+    Both you and the comuter will place your ships on the given board.
+
+    Once all the ships are palced both you and the computer will take turns making
+    guesses where the other's ships are.  Guesses are made using grid positions, similar
+    to how you placed your ships on the board.
+
+    the game is over once either you or the comuter has successfully sunk the
+    opponent's ships.
+
+    Good luck and have fun!\n\n"
+  end
+
+  
+
+end
