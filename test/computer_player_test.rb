e0c804880d1123c91c24c6be15f0c6a235ae5a2a
Ben Pepper <bfpepper87@gmail.com>
2016-07-19 15:51:18 -0600

    more things

--- test/computer_player_test.rb
+++ test/computer_player_test.rb
@@ -47,6 +47,19 @@ class ComputerPlayerTest < Minitest::Test
     assert "H", player.human_board.internal_board
   end
 
+  def test_can_select_ramdom_ship_placment
+    results = []
+    5.times{ results << ComputerPlayer.new.random_placement(2) }
+    assert results.uniq.count > 1
+    5.times{ results << ComputerPlayer.new.random_placement(3) }
+    assert results.uniq.count > 1
+  end
 
+  def test_can_place_selected_coordinates_on_a_board
+    player = ComputerPlayer.new
+    board = ComputerPlayer.new.computer_board.full_board
+    player.place_ship(2)
+    refute_equal board, player.computer_board.internal_board
+  end
 
 end
